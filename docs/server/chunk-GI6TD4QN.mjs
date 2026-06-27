d6a57e54d6d227dcb291f9cbda7c1fc921b3f65d
amira-Selim <amiraselem2004@email.com>
2026-06-27 05:52:46 +0300

    Save current state before Company role feature

--- docs/server/chunk-GI6TD4QN.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-var t={production:!0,baseUrl:"https://uccdapp.runasp.net"};export{t as a};
--- src/app/core/environments/environment.prod.ts
+++ src/app/core/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://uccdapp.runasp.net'
+  baseUrl: ''
 };
